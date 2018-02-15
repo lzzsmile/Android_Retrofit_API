8ac9630ff8bbc38e87e6b59dc95c5a8f8fdb06bd
lzzsmile <32747784+lzzsmile@users.noreply.github.com>
2018-02-14 23:15:47 -0700

    Create ListWrapper class to wrap a list of objects of the given parameter's type.

---
+++ retrofitStackoverflow/app/src/main/java/zhuangzhi/android/retrofitstackoverflow/ListWrapper.java
@@ -0,0 +1,11 @@
+package zhuangzhi.android.retrofitstackoverflow;
+
+import java.util.List;
+
+/**
+ * Created by zhuangzhili on 2018-02-14.
+ */
+
+public class ListWrapper<T> {
+    List<T> items;
+}
